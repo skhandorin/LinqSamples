8ab91debb69e2f3d67e823752749c23c6d1dfee8
skhandorin <hsatig@mail.ru>
2017-04-12 15:12:44 +0500

    Module 4 - Queries

---
+++ Queries/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Queries
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public float Rating { get; set; }
+
+        int _year;
+        public int Year {
+            get
+            {
+                //throw new Exception("Error in Movie.Year getter!");
+                Console.WriteLine($"Returning {_year} for {Title}");
+                return _year;
+            }
+            set
+            {
+                _year = value;
+            }
+        }
+
+    }
+}
