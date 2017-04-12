15ee0187a5609e9a562e77f2d33a8c7534ddf7eb
skhandorin <hsatig@mail.ru>
2017-04-12 16:14:51 +0500

    Module 5 - Cars from CSV

---
+++ Cars/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined { get; set; }
+
+        private static NumberFormatInfo _nfi = new CultureInfo("en-US", false).NumberFormat;
+
+        internal static Car ParseFromCsv(string line)
+        {
+            var columns = line.Split(',');
+
+            return new Car
+            {
+                Year = int.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Name = columns[2],
+                Displacement = double.Parse(columns[3], _nfi),
+                Cylinders = int.Parse(columns[4]),
+                City = int.Parse(columns[5]),
+                Highway = int.Parse(columns[6]),
+                Combined = int.Parse(columns[7])
+            };
+        }
+    }
+}
---
+++ Cars/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            //return File.ReadAllLines(path)
+            //            .Skip(1)
+            //            .Where(line => line.Length > 1)
+            //            .Select(Car.ParseFromCsv)
+            //            .ToList();
+
+            var query =
+                from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+            return query.ToList();
+        }
+
+
+    }
+}
